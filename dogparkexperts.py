2512c8a83a2e406f29564d279e52b6eab84e12c5
Tyler <tplaxstar@gmail.com>
2024-07-31 22:45:37 -0700

    window clear figured out

--- dogparkexperts.py
+++ dogparkexperts.py
@@ -6,43 +6,43 @@ from tkinter import *
 from tkinter.ttk import *
 
 # creates a Tk() object
-master = Tk()
+window = Tk()
  
 # sets the geometry of main 
 # root window
-master.geometry("600x400")
+window.geometry("750x500")
  
 # function to open a new window 
-# on a button click
-def openNewWindow():
-     
-    # Toplevel object which will 
-    # be treated as a new window
-    newWindow = Toplevel(master)
+# on a button clic8
+def mainPage():
+    clear()
+    welcomeLabel = Label(window, 
+              text ="Here are our favorites:")
+    welcomeLabel.pack(pady=10)
+    
+
+# citation for the following function:
+# Date: 07/31/24
+# copied from:
+# https://stackoverflow.com/questions/15781802/python-tkinter-clearing-a-frame
+def clear():
+    for widget in window.winfo_children():
+        widget.destroy()
+
  
-    # sets the title of the
-    # Toplevel widget
-    newWindow.title("New Window")
- 
-    # sets the geometry of toplevel
-    newWindow.geometry("200x200")
- 
-    # A Label widget to show in toplevel
-    Label(newWindow, 
-          text ="This is a new window").pack()
- 
- 
-label = Label(master, 
+def login():
+    guestLabel = Label(window, 
               text ="Welcome to Dog Park Experts")
  
-label.pack(pady = 10)
+    guestLabel.pack(pady = 10)
  
 # a button widget which will open a 
 # new window on button click
-btn = Button(master, 
+    btn = Button(window, 
              text ="Continue as Guest", 
-             command = openNewWindow)
-btn.pack(pady = 10)
+             command = mainPage)
+    btn.pack(pady = 10)
  
 # mainloop, runs infinitely
+login()
 mainloop()
