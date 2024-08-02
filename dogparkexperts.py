7079ef24e0cca70bcffe21b87cfe78d6b8e7f74e
Tyler <tplaxstar@gmail.com>
2024-08-01 19:29:49 -0700

    all labels and buttons

--- dogparkexperts.py
+++ dogparkexperts.py
@@ -15,68 +15,195 @@ window.title("Dog Park Experts")
 
 def mainPage():
     clear()
-    favoritesLabel = Label(window, 
-              text ="Here are our favorites:")
-    favoritesLabel.pack(pady=10)
     userInfoBtn = Button(window, 
-             text ="User Info", 
+             text ="Track your parks: User Info", 
              command = userPage)
-    userInfoBtn.pack(pady = 10)
+    
+    # citation for the follow anchor syntax:
+    # Date: 08/01/24
+    # Adapted from:
+    # https://stackoverflow.com/questions/58016467/how-to-make-tkinterpack-place-label-on-top-left-corner-in-below-program
+    userInfoBtn.pack(pady=0, anchor=NE)
 
     logoutBtn = Button(window, 
              text ="Log out", 
              command = loginPage)
-    logoutBtn.pack(pady = 10)
+    logoutBtn.pack(pady = 0,anchor=NE)
+
+    tagsLabel = Label(window, 
+              text ="Sort by Tags:")
+    tagsLabel.pack(padx=10, pady=10, anchor=NW)
+
+    tagsText= Text(window, height=1, width=20)
+    tagsText.pack(padx=10, pady=10, anchor=NW)
+
+    tagBtn = Button(window, 
+             text ="Suggested tags here", 
+             command = mainPage)
+    tagBtn.pack(padx=10, pady = 10, anchor=NW)
+
+    favoritesLabel = Label(window, 
+              text ="Here are our favorites:")
+    favoritesLabel.pack(pady=10)
+
+    parkLeftArrow = Button(window, 
+             text ="<-", 
+             command = parkPage)
+    parkLeftArrow.pack(pady = 10, side=LEFT, padx= 95)
+
+    parkBtnOne = Button(window, 
+             text ="Park", 
+             command = parkPage)
+    parkBtnOne.pack(pady = 10, side=LEFT)
+    parkBtnTwo = Button(window, 
+             text ="Park", 
+             command = parkPage)
+    parkBtnTwo.pack(pady = 10, side=LEFT)
+    parkBtnThree = Button(window, 
+             text ="Park", 
+             command = parkPage)
+    parkBtnThree.pack(pady = 10, side=LEFT)
+
+    parkRightArrow = Button(window, 
+             text ="->", 
+             command = parkPage)
+    parkRightArrow.pack(pady = 10, side=LEFT, padx=55)
 
     addParkBtn = Button(window, 
              text ="Add a Park", 
              command = addParkPage)
-    addParkBtn.pack(pady = 10)
-    parkBtn = Button(window, 
-             text ="Park", 
-             command = parkPage)
-    parkBtn.pack(pady = 10)
+    addParkBtn.pack(pady = 0, side=BOTTOM, anchor=SE)
 
 def parkPage():
     clear()
     backBtn = Button(window, 
              text ="Back", 
              command = mainPage)
-    backBtn.pack(pady = 10)
+    backBtn.pack(pady = 0, anchor=NE)
+
+    tagsLabel = Label(window, 
+              text ="Tags:")
+    tagsLabel.pack(pady = 10)
+
+    reviewsLabel = Label(window, 
+              text ="Reviews:")
+    reviewsLabel.pack(pady = 10)
+
     reviewBtn = Button(window, 
              text ="Add a Review", 
              command = reviewPage)
-    reviewBtn.pack(pady = 10)
+    nameLabel = Label(window, 
+              text ="Park Name")
+    nameLabel.pack(pady=10)
+    reviewBtn.pack(pady = 0, side=BOTTOM, anchor=SE)
 
 def reviewPage():
     clear()
     backBtn = Button(window, 
              text ="Back/Cancel", 
              command = parkPage)
-    backBtn.pack(pady = 10)
+    backBtn.pack(pady = 0, anchor=NE)
+
+    thankYouLabel = Label(window, 
+              text ="Thank you for Reviewing Park Name!")
+    thankYouLabel.pack(pady=10)
+
+    ratingLabel = Label(window, 
+              text ="Rating out of 5:")
+    ratingLabel.pack(pady = 10)
+    ratingText= Text(window, height=1, width=20)
+    ratingText.pack()
+
+    tagsLabel = Label(window, 
+              text ="Tags (please separate by a comma):")
+    tagsLabel.pack(pady = 10)
+    tagsText= Text(window, height=1, width=20)
+    tagsText.pack()
+
+    picLabel = Label(window, 
+              text ="Picture:")
+    picLabel.pack(pady = 10)
+    picText= Text(window, height=1, width=20)
+    picText.pack()
+
     submitBtn = Button(window, 
              text ="Submit", 
              command = parkPage)
-    submitBtn.pack(pady = 10)
-
+    submitBtn.pack(pady = 0, side=BOTTOM, anchor=S)
 
 def userPage():
     clear()
     backBtn = Button(window, 
              text ="Back", 
              command = mainPage)
-    backBtn.pack(pady = 10)
+    backBtn.pack(pady = 0, anchor=NE)
+
+    usernameLabel = Label(window, 
+              text ="Welcome Username!")
+    usernameLabel.pack(pady=10)
+
+    favoriteLabel = Label(window, 
+              text ="Favorite Parks:")
+    favoriteLabel.pack(pady=10)
+
+    visitedLabel = Label(window, 
+              text ="Visited Parks:")
+    visitedLabel.pack(pady=10)
+
+    yourParksLabel = Label(window, 
+              text ="Your Parks:")
+    yourParksLabel.pack(pady=10)
+
+    yourPicturesLabel = Label(window, 
+              text ="Your Pictures:")
+    yourPicturesLabel.pack(pady=10)
+
+    reviewLabel = Label(window, 
+              text ="Your Reviews:")
+    reviewLabel.pack(pady=10)
+
+    accountInfoLabel = Label(window, 
+              text ="This information will be saved even after you log out!")
+    accountInfoLabel.pack(pady=10)
+
+    deleteAccountBtn = Button(window, 
+             text ="Delete Account", 
+             command = userPage)
+    deleteAccountBtn.pack(pady = 0, side=BOTTOM, anchor=SE)
 
 def addParkPage():
     clear()
     backBtn = Button(window, 
              text ="Back", 
              command = mainPage)
-    backBtn.pack(pady = 10)
+    backBtn.pack(pady = 0, anchor=NE)
+
+    thankYouLabel = Label(window, 
+              text ="Thank you for Adding a Park!")
+    thankYouLabel.pack(pady=10)
+
+    nameLabel = Label(window, 
+              text ="Park Name:")
+    nameLabel.pack(pady = 10)
+    nameText= Text(window, height=1, width=20)
+    nameText.pack()
+
+    passLabel = Label(window, 
+              text ="Location:")
+    passLabel.pack(pady = 10)
+    passText= Text(window, height=1, width=20)
+    passText.pack()
+
+    picLabel = Label(window, 
+              text ="Picture:")
+    picLabel.pack(pady = 10)
+    picText= Text(window, height=1, width=20)
+    picText.pack()
+
     submitBtn = Button(window, 
-             text ="Submit", 
+             text ="Add Park", 
              command = mainPage)
-    submitBtn.pack(pady = 10)
+    submitBtn.pack(pady = 0,side=BOTTOM, anchor=S)
 
 # citation for the following function:
 # Date: 07/31/24
@@ -116,15 +243,19 @@ def loginPage():
              text ="Log in", 
              command = mainPage)
     loginBtn.pack(pady = 10)
+    createAccountBtn = Button(window, 
+             text ="Create an Account", 
+             command = mainPage)
+    createAccountBtn.pack(pady = 10)
 
     
     accountFeatureLabel = Label(window, 
               text ="Create an account to track your visited parks or review and add new ones!")
-    accountFeatureLabel.pack(pady=100)
+    accountFeatureLabel.pack(pady=10)
     guestBtn = Button(window, 
              text ="Continue as Guest", 
              command = mainPage)
-    guestBtn.pack(pady = 10)
+    guestBtn.pack(pady = 90)
     
  
 # mainloop, runs infinitely
